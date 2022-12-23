5109d8296becf65f8f7dcba238332f04bd7423a8
PowerOleg <geraskin.oleg@mail.ru>
2022-12-24 04:04:13 +0500

    1.0-snapshot

---
+++ src/main/java/ru/netology/http/httpCat.java
@@ -0,0 +1,7 @@
+package ru.netology.http;
+
+public class httpCat {
+    public static void main( String[] args ) {
+
+    }
+}
---
+++ src/test/java/ru/netology/http/httpCatTest.java
@@ -0,0 +1,20 @@
+package ru.netology.http;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class httpCatTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
