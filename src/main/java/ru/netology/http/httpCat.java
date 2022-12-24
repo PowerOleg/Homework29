fdd6015d1a2f7ae4041605e56d9b9061aede2bed
PowerOleg <geraskin.oleg@mail.ru>
2022-12-24 17:30:42 +0500

    HttpGet request 1.1-snapshot

--- src/main/java/ru/netology/http/httpCat.java
+++ src/main/java/ru/netology/http/httpCat.java
@@ -1,7 +1,27 @@
 package ru.netology.http;
 
-public class httpCat {
-    public static void main( String[] args ) {
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpRequest;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 
+public class httpCat {
+    public static final String REMOTE_URI = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    public static void main(String[] args) {
+
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setUserAgent("Oleg")
+                .setDefaultRequestConfig(RequestConfig.custom()
+                .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build()).build();
+
+        HttpGet request = new HttpGet(REMOTE_URI);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
     }
 }
