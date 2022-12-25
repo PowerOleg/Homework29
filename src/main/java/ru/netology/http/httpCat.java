d74a7d734a493dc48747f7b9c0a025cf361da6ac
PowerOleg <geraskin.oleg@mail.ru>
2022-12-25 07:00:55 +0500

    testHeaders 1.3-snapshot

--- src/main/java/ru/netology/http/httpCat.java
+++ src/main/java/ru/netology/http/httpCat.java
@@ -2,19 +2,22 @@ package ru.netology.http;
 
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpRequest;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 public class httpCat {
     public static final String REMOTE_URI = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
     public static void main(String[] args) {
-
-
         CloseableHttpClient httpClient = HttpClientBuilder.create()
-                .setUserAgent("Oleg")
+                .setUserAgent("Yandex/22.11.3.818 (64-bit)")
                 .setDefaultRequestConfig(RequestConfig.custom()
                 .setConnectTimeout(5000)
                         .setSocketTimeout(30000)
@@ -23,5 +26,21 @@ public class httpCat {
 
         HttpGet request = new HttpGet(REMOTE_URI);
         request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+        request.addHeader(HttpHeaders.ACCEPT_ENCODING, "gzip");
+        request.addHeader(HttpHeaders.ACCEPT_LANGUAGE, "en");
+//        httpURLConnection.addRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+        try (CloseableHttpResponse response = httpClient.execute(request)) {
+            Arrays.stream(response.getAllHeaders()).forEach(n -> System.out.println(n));
+
+
+
+        } catch (ClientProtocolException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
