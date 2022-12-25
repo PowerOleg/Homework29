16ece73894cca94c080253a05b8d82433ee79af5
PowerOleg <geraskin.oleg@mail.ru>
2022-12-25 07:53:00 +0500

    Got response 1.4-snapshot

--- src/main/java/ru/netology/http/CloseableHttpClient.java
+++ src/main/java/ru/netology/http/CloseableHttpClient.java
@@ -1,22 +1,24 @@
 package ru.netology.http;
 
 import org.apache.http.HttpHeaders;
-import org.apache.http.HttpRequest;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.entity.ContentType;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
-public class httpCat {
+public class CloseableHttpClient {
     public static final String REMOTE_URI = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    public static final ObjectMapper jsonMapper = new ObjectMapper();
     public static void main(String[] args) {
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
+        org.apache.http.impl.client.CloseableHttpClient httpClient = HttpClientBuilder.create()
                 .setUserAgent("Yandex/22.11.3.818 (64-bit)")
                 .setDefaultRequestConfig(RequestConfig.custom()
                 .setConnectTimeout(5000)
@@ -33,6 +35,8 @@ public class httpCat {
         try (CloseableHttpResponse response = httpClient.execute(request)) {
             Arrays.stream(response.getAllHeaders()).forEach(n -> System.out.println(n));
 
+            List<Cat> cats = jsonMapper.readValue(response.getEntity().getContent(), new TypeReference<List<Cat>>() {});
+            cats.forEach(n -> System.out.println(n));
 
 
         } catch (ClientProtocolException e) {
--- src/test/java/ru/netology/http/CloseableHttpClientTest.java
+++ src/test/java/ru/netology/http/CloseableHttpClientTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class httpCatTest
+public class CloseableHttpClientTest
 {
     /**
      * Rigorous Test :-)
